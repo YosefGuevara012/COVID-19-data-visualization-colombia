d46f40b251dc8548417ece2d6d61ab60d982ee19
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2020-03-20 17:32:41 -0500

    Se agrega el mapa

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+var map = L.map('map').setView([4.7106778, -74.0732425], 5);
+    L.tileLayer('https://api.maptiler.com/maps/darkmatter/{z}/{x}/{y}.png?key=9aOW9P5TIthZlr4M7tLF', {attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'}).addTo(map);
